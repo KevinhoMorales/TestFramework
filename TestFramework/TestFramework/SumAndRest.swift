bf25f715cb423dc847a73c26eddbfe55e043e417
Kevin Morales <kfmorales94@gmail.com>
2021-03-03 14:49:49 -0500

    Sum and rest func added

---
+++ TestFramework/TestFramework/SumAndRest.swift
@@ -0,0 +1,26 @@
+//
+//  SumAndRest.swift
+//  TestFramework
+//
+//  Created by Kevinho Morales on 3/3/21.
+//
+
+import Foundation
+
+public class SumAndRest {
+    
+//    private init(){
+//        
+//    }
+    
+    public static func sum(one: Double, two: Double) -> Double {
+        let result = one + two
+        return result
+    }
+    
+    public static func rest(one: Double, two: Double) -> Double {
+        let result = one - two
+        return result
+    }
+    
+}
